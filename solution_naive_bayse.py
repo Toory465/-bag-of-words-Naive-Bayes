a27638c58b4bc20d212874c5d914f7b717e5db46
toory465 <touraj.laleh@gmail.com>
2019-11-18 23:10:10 -0500

    final version

--- solution_naive_bayse.py
+++ solution_naive_bayse.py
@@ -5,18 +5,28 @@ from nltk.probability import FreqDist
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import math
 
+## Skleran liberary for the second phayse of the competition
+from sklearn import model_selection, naive_bayes, svm
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import ComplementNB
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.svm import LinearSVC
 
 # Classification Models
-# train_data_path = '../data_train.pkl'
-# test_data_path = '../data_test.pkl'
-# final_output_path_nb = '../result_naive_bayse.csv'
-# final_output_path_nb_lp = '../result_naive_bayse_lp_2.csv'
+train_data_path = './data_train.pkl'
+test_data_path = '../data_test.pkl'
+final_output_path_nb = '../result_naive_bayse.csv'
+final_output_path_nb_lp = '../result_naive_bayse_lp.csv'
+final_output_path = '../result.csv'
 
+
+### the  Parent Classifier class
 class Classifier:
-    """
-    The Classifier super class
-    """
     def __init__(self):
         df_train, df_test = self._data_loader()
         self.train_data = df_train
@@ -25,7 +35,7 @@ class Classifier:
         self.topic_set = self.train_data['Id'].unique()
 
     def _data_loader(self):
-        test_data = np.load(test_data_path , allow_pickle=True)
+        test_data = np.load(test_data_path, allow_pickle=True)
         train_data = np.load(train_data_path, allow_pickle=True)
         dt_train = pd.DataFrame(list(zip(train_data[0], train_data[1])), columns=['Category', 'Id'])
         dt_test = pd.DataFrame(test_data, columns=['Category'])
@@ -36,6 +46,8 @@ class Classifier:
         final_dt = pd.DataFrame(list(zip(prediction_vector, range_list)), columns=['Category', 'Id'])
         final_dt.to_csv(final_output_path)
 
+
+# the Random Classifier
 class RandomClassifier(Classifier):
 
     def __init__(self):
@@ -46,11 +58,13 @@ class RandomClassifier(Classifier):
         result_dt['Category'] = np.random.randint(0, self.number_of_classes, size=self.test_data.shape[0])
         return result_dt
 
+
+## The Naive base Classifier
 class NaiveBayesClassifier(Classifier):
 
     def __init__(self):
         super().__init__()
-        self.alpha = 2.15
+        self.alpha = 2.25
         sent_set = set()
         for sentence in self.train_data['Category']:
             sent = re.sub("[^\w]", " ", sentence).split()
@@ -58,7 +72,9 @@ class NaiveBayesClassifier(Classifier):
             for item in list_:
                 sent_set.add(item)
         self.train_data_vocab_count = len(sent_set)
+        self.Encoder = LabelEncoder()
 
+    # Data Cleaning part
     def _word_extraction(self, sentence):
         stop_wrods = set(stopwords.words('english'))
         porter = PorterStemmer()
@@ -69,11 +85,8 @@ class NaiveBayesClassifier(Classifier):
         stemmed = [porter.stem(word) for word in cleaned_text]
         return stemmed
 
+    # implementaiton of simple Naive Bayse CLassifier
     def Naive_Bayse_probability_calculator(self):
-        """
-        Naive base classifer function
-        :return:
-        """
         dict = {}
         for topic in self.topic_set:
             dt = self.train_data[self.train_data['Id'] == topic]
@@ -86,12 +99,10 @@ class NaiveBayesClassifier(Classifier):
             dict[topic] = topic_freq_series
         return dict
 
+    ## Implementation of Naive Bayse Classifier with Laplace smoothing
     def Naive_Bayse_probability_calculator_laplas_smothing(self):
-        """
-        naive base classifier with laplas smoothing function
-        :return:
-        """
         dict = {}
+        topic_word_count = {}
         for topic in self.topic_set:
             dt = self.train_data[self.train_data['Id'] == topic]
             topic_words = [item for row in dt['extract_words'] for item in row]
@@ -103,26 +114,34 @@ class NaiveBayesClassifier(Classifier):
             dict[topic] = topic_freq_series
         return dict
 
-   
+    # The prediction Method
     def prediction(self, dict_):
         dt_test_category = self.test_data['Category'].apply(self._word_extraction)
         prediction_vector = []
         for i, sentence in enumerate(dt_test_category):
             score_dict = {}
             for topic in self.topic_set:
-                topic_score = 1.0
+                topic_score = 0.0
+
+                # topic_score = 1.0    /// NAive bayse approch
                 for word in sentence:
-                    if dict_[topic][word] > 0.0 : 
+                    if dict_[topic][word] > 0.0:
                         probability = dict_[topic][word]
                     else:
                         probability = (1.0 / self.train_data_vocab_count)
-                    topic_score = topic_score * probability
-                    if topic_score == 0:
-                        topic_score = 0.00001
+
+                    ### Caclulate Log probabilities
+                    topic_score = topic_score + math.log(probability)
+
+                    ### Caclulate pure naive bayse appraoch
+                    ## topic_score = topic_score * probability
+                #                     if topic_score == 0:  /// NAive bayse approch
+                #                         topic_score = 0.00001  /// NAive bayse approch
                 score_dict[topic] = topic_score
             prediction_vector.append(max(score_dict, key=score_dict.get))
         return prediction_vector
 
+    # the classifier method
     def classifier(self):
         dt_train_category = self.train_data['Category'].apply(self._word_extraction)
         self.train_data['extract_words'] = dt_train_category
@@ -132,15 +151,57 @@ class NaiveBayesClassifier(Classifier):
         # prediction = self.prediction(dict_nb)
         # self._csv_output_generator(prediction, final_output_path_nb)
 
-        #Naive bayse Laplas classifier
+        # Naive bayse Laplas classifier
         dict_nb_lp = self.Naive_Bayse_probability_calculator_laplas_smothing()
         prediction = self.prediction(dict_nb_lp)
-        self._csv_output_generator(prediction, final_output_path_nb_lp) 
+        self._csv_output_generator(prediction, final_output_path_nb_lp)
+
+    def _sklearn_data_cleaning(self):
+        Train_Y = self.Encoder.fit_transform(self.train_data['Id'])
+        Tfidf_vect = TfidfVectorizer()
+        Tfidf_vect.fit(self.train_data['Category'])
+        Train_X_Tfidf = Tfidf_vect.transform(self.train_data['Category'])
+        Test_X_Tfidf = Tfidf_vect.transform(self.test_data['Category'])
+        return Train_X_Tfidf, Test_X_Tfidf, Train_Y
+
+    def guassian_distribution_classifier(self):
+
+        Train_X_Tfidf, Test_X_Tfidf, Train_Y = self._sklearn_data_cleaning()
+        gnb = ComplementNB(alpha=1.590)
+        gnb.fit(Train_X_Tfidf, Train_Y)
+        # predict the labels on validation dataset
+        predictions_NB = gnb.predict(Test_X_Tfidf)
+
+        range_list = [item for item in range(0, len(self.test_data))]
+        final_dt = pd.DataFrame(list(zip(self.Encoder.inverse_transform(predictions_NB), range_list)),
+                                columns=['Category', 'Id'])
+        self._csv_output_generator(final_dt, final_output_path)
+
+        ## Support Vecto Machine
+
+    def SVM(self):
+
+        Train_X_Tfidf, Test_X_Tfidf, Train_Y = self._sklearn_data_cleaning()
+        model = LinearSVC()
+        model.fit(Train_X_Tfidf, Train_Y)
+
+        # predict the labels on validation dataset
+        predictions_NB = model.predict(Test_X_Tfidf)
+
+        range_list = [item for item in range(0, len(self.test_data))]
+        final_dt = pd.DataFrame(list(zip(self.Encoder.inverse_transform(predictions_NB), range_list)),
+                                columns=['Category', 'Id'])
+        self._csv_output_generator(final_dt, final_output_path)
 
 
-# In[104]:
-
-
+## define the classifier object
 nbc = NaiveBayesClassifier()
+
+### NAive bayse classifier using laplac smoothing and log probabilities
 pred = nbc.classifier()
 
+## Classification using Guassian Distribution
+pred = nbc.guassian_distribution_classifier()
+
+## CLasiification Using SVM
+pred = nbc.SVM()
